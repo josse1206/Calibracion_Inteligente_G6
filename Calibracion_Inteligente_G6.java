3c1a675136a2340a9ba249dd8c913044c7e00bab
josse1206 <pepejuse41@gmail.com>
2020-05-27 21:49:05 -0500

    coreccion del codigo 5

--- Calibracion_Inteligente_G6.java
+++ Calibracion_Inteligente_G6.java
@@ -29,14 +29,15 @@ public class Calibracion_Inteligente_G6 {
         System.out.println("Modelo computacional");
         System.out.println("Caso practico Robots en la Industria Automotriz");
         System.out.println("Calibracion Inteligente");
-        System.out.println("6. Registro del Nivel ERP");
-        String erp=objeto.nextLine();
-        
+
         System.out.println("5. Registro del Nivel MES");
         String mes=objeto.nextLine();
+
+        System.out.println("6. Registro del Nivel ERP");
+        String erp=objeto.nextLine();
         
         System.out.println("7. Imprimir Reportes");
         String reportes=objeto.nextLine();
     }
-    
+
 }
